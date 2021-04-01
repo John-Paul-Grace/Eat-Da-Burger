b2400f1c3e61703077f366c009935d604b71ca6f
JP-Grace <gracejohnpaul200@gmail.com>
2021-03-31 23:04:48 -0400

    orm selectAll() added

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,17 @@
 const connection = require("./connection.js");
 
 const orm = {
-    
+    selectAll(tableInput, cb) {
+        const queryString = "SELECT * FROM ??";
+
+        connection.query(queryString, [tableInput], (err, result) => {
+            if (err) throw err;
+
+            cb(result);
+        });
+    },
+    insertOne() {},
+    updateOne() {}
 };
 
 module.exports = orm;
