b899ee75e0fddcef6c344e86cc8ffea9ae5bd4eb
JP-Grace <gracejohnpaul200@gmail.com>
2021-04-07 18:10:37 -0400

    added some comments and removed some console.logs

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,21 +1,28 @@
+// Imports necessary files and libraries
 const burger = require("../models/burger.js");
-
 const express = require("express");
 const router = express.Router();
 
+// Api GET route
 router.get("/", (req, res) => {
+    // Calls the burger.all() function from the burger model
     burger.all((data) => {
+        // Creates an object to be used by handlebars
         const hbsObject = {
             burgers: data
         }
 
+        // Logs the handlebars object on the server side
         console.log(hbsObject);
 
+        // Renders index.handlebars using the handlebars object
         res.render("index", hbsObject);
     });
 });
 
+// Api CREATE route
 router.post("/api/add", (req, res) => {
+    // Calls the burger.create() function from the burger model
     burger.create(req.body.name, (result) => {
         console.log(`${result.affectedRows} rows affected`);
         res.json(result);
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,21 +4,30 @@ document.addEventListener('DOMContentLoaded', (readyEvent) => {
         console.info('DOM loaded');
     }
 
+    // CREATE
+    // ==================================================================================
+    // Creates a reference to the submit button element
     const submitBtn = document.querySelector("#burger-submit");
 
+    // Adds a click event listener to the submit button
     submitBtn.addEventListener("click", (event) => {
+        // Prevents the default reloading of the page
         event.preventDefault();
 
+        // Creates a reference to the text input element
         const inputArea = document.getElementById("burger-name-input");
 
+        // Gets the value of the text input
         const input = inputArea.value.trim();
 
+        // Checks if the input is empty or not
         if (input) {
-
+            // Puts the input into an object to be used as the body for the api call
             const inputObj = {
                 name: input
             }
 
+            // Executes the post api call to add a burger to the list
             fetch("/api/add", {
                 method: "POST",
                 headers: {
@@ -27,30 +36,38 @@ document.addEventListener('DOMContentLoaded', (readyEvent) => {
                 },
                 body: JSON.stringify(inputObj)
             }).then(() => {
+                // Clears the input area
                 inputArea.value = "";
 
-                console.log("Posted!");
+                // Reloads the page
                 location.reload();
             });
         }
+        // If input is empty, the user is told to enter a name
         else {
-            alert("Please input a name.");
+            alert("Please enter a name for your burger");
         }
     });
+    // ==================================================================================
 
+    // UPDATE
+    // ==================================================================================
+    // Creates an array of references to all of the "devour" buttons
     const devourBtns = document.querySelectorAll(".devour");
 
+    // Adds an event listener to each button referenced in the devourBtns array
     devourBtns.forEach((btn) => {
         btn.addEventListener("click", (event) => {
+            // Gets the id from the "data-id" attribute of each button
             const id = event.target.getAttribute("data-id");
 
-            console.log("id = " + id);
-
+            // Creates an object with column to update and data to be updated to
             const bodyObj = {
                 col: "devoured",
                 data: true
             };
 
+            // Performs the api update call
             fetch(`/api/update/${id}`, {
                 method: "PUT",
                 headers: {
@@ -58,10 +75,8 @@ document.addEventListener('DOMContentLoaded', (readyEvent) => {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify(bodyObj)
-            }).then(() => {
-                console.log("Updated!");
-                location.reload();
-            });
+            }).then(() => location.reload()); // Reloads the page after updating db
         });
     });
+    // ==================================================================================
 });
